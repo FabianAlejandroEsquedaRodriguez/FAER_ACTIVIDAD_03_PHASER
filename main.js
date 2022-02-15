12fa2ab878269b221479ee451ad103c85f73ada6
Fabián Alejandro Esqueda Rodríguez <fabian.esqueda2892@alumnos.udg.mx>
2022-02-15 11:43:08 -0600

    Funcionalidad del juego; tiene ambos metodos de entrada, por teclado y mouse

---
+++ main.js
@@ -0,0 +1,169 @@
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var platforms;
+var player;
+var cursors;
+var stars;
+var score = 0;
+var scoreText;
+var congrats;
+var bombs;
+var gameOver = false;
+var gameOverText;
+
+// //Crear un nuevo objeto de la clase Phaser
+var game = new Phaser.Game(config);
+
+function preload () {
+    // this.load.image('ship', '/assets/star.png');
+    this.load.image('sky', '/assets/sky.png');
+    this.load.image('ground', '/assets/platform.png');
+    this.load.image('star', '/assets/star.png');
+    this.load.image('bomb', '/assets/bomb.png');
+    this.load.spritesheet('dude', '/assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+}
+
+var sprite;
+function create() {
+    this.add.image(400, 300, 'sky');
+    // Crear las plataformas dandoles fisica
+    platforms = this.physics.add.staticGroup();
+    platforms.create(400,568,'ground').setScale(2).refreshBody();
+    platforms.create(600,400,'ground');
+    platforms.create(50,250,'ground');
+    platforms.create(750,220,'ground');
+    // sprite = this.add.sprite(400, 300, 'dude');
+    player = this.physics.add.sprite(100,450,'dude');
+
+    //Dandole funcionalidad al monito
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'turn',
+        frames: [{key: 'dude', frame: 4}],
+        frameRate: 20
+    });
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    
+    //Para poder mover al personaje con las flechitas
+    cursors = this.input.keyboard.createCursorKeys();
+     // Cuando se presione dentro del cielo, el puntero se bloqueará-> 'engagement gesture', e.g. mousedown, keypress, etc.
+    // this.input.on('pointerdown', function (pointer) {
+    //     this.input.mouse.requestPointerLock();
+    // }, this);
+    //Para mover el personaje con el mouse
+    this.input.on('pointermove', function (pointer){
+        player.x = pointer.x;
+        player.y = pointer.y;
+        if(gameOver){ //Si toca una bomba, la animacion se pausa
+            this.physics.pause();
+        }
+    });
+
+    //Creando las estrellas
+    stars = this.physics.add.group({
+        key: 'star',
+        repeat: 11,
+        setXY: {x:12, y:0, stepX:70}
+    });
+    stars.children.iterate(function(child){
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8))
+    });
+
+    //Crear las bombas de rebote
+    bombs = this.physics.add.group();
+    
+    //variable de puntuacion
+    scoreText = this.add.text(16,16, 'SCORE: 0', {fontSize:'32px', fill:'#fff'});
+    // levelText = this.add.text(250,16, 'LEVEL: 1', {fontSize:'32px', fill:'#fff'})
+
+    //Para que el personaje, bombas y estrellas interactue con las plataformas
+    this.physics.add.collider(player, platforms);
+    this.physics.add.collider(stars, platforms);
+    this.physics.add.collider(bombs, platforms);
+
+    //comprobar si el personaje se superpone con alguna estrella o bomba
+    this.physics.add.overlap(player, stars, collectStar, null, this);
+    this.physics.add.collider(player, bombs, hitBomb, null, this);
+}
+
+function update () { 
+    if (gameOver){
+        gameOverText = this.add.text(230,300, 'GAME OVER :(', {fontSize:'40px', fill:'#000'});
+        return;
+    }
+    if (cursors.left.isDown) {//Si la flecha izq está presionada
+        player.setVelocityX(-160);
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown){//Si la flecha der está presionada
+        player.setVelocityX(160);
+        player.anims.play('right', true);
+    }
+    else {//si no se está presionando ninguna flecha el monito se voltea de frente
+        player.setVelocityX(0);
+        player.anims.play('turn');
+    }
+    if (cursors.up.isDown && player.body.touching.down) {//Si se esta presionando la flecha hacia arriba  
+        player.setVelocityY(-330);
+    }
+}
+
+//Para que el personaje recolecte estrellas y desaparezcan
+function collectStar (player, star) {
+    star.disableBody(true, true);
+    score += 10;
+    scoreText.setText(`SCORE:${score}`);
+    // level +=1;
+    // levelText.setText(`LEVEL:${level}`);
+
+    //Para que aparezca una bomba por cada 120pts sumados
+    if (stars.countActive(true) === 0) {
+        stars.children.iterate(function(child){
+            child.enableBody(true, child.x, 0, true, true);
+        });
+        var x = (player.x < 400) ? Phaser.Math.Between(400, 800): Phaser.Math.Between(0,400);
+        var bomb = bombs.create(x, 16, 'bomb');
+        bomb.setBounce(1);
+        bomb.setCollideWorldBounds(true);
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+        bomb.allowGravity = false;
+    }
+}
+
+//Para determinar si el personaje choca con alguna bomba
+function hitBomb(player, bomb) {
+    this.physics.pause();
+    player.setTint(0xff0000);
+    player.anims.play('turn');
+    gameOver = true;
+}
